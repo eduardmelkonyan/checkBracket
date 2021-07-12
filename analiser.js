90ac830da80e86fe2f9dfecc00e42cd96ba245e2
eduardmelkonyan <melkon1999@gmail.com>
2021-07-12 18:05:24 +0400

    aaa

---
+++ analiser.js
@@ -0,0 +1,5 @@
+class Stack{
+    constructor(){};
+    arr = [];
+    }
+module.exports = Stack;
---
+++ analiser1.js
@@ -0,0 +1,33 @@
+const Stack = require('./analiser')
+
+const stack = new Stack;
+const arr1 = stack.arr; 
+
+class Check{
+    constructor(){}
+    arr1 = [];
+bracket(string){
+    for(let i = 0; i < string.length; ++i){
+        if(string[i] == '(' || string[i] == '{' || string[i] == '['){
+            this.arr1.push(string[i]);
+        }
+    if(this.arr1[this.arr1.length-1] == '(' && string[i] == ')' || this.arr1[this.arr1.length-1] == '{' && string[i] == '}' || this.arr1[this.arr1.length-1] == '[' && string[i] == ']'  ){
+        this.arr1.pop();
+    }
+}
+if(this.arr1.length == 0){
+    console.log(true);
+}else{
+    throw new Error('Sxal');
+}
+}
+}
+
+
+try{
+   const check = new Check();
+check.bracket('[{()}]');
+}
+catch(e){
+    console.log(e.message, e)
+}
